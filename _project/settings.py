3bb100ae90fed0b515a771a0a839c28e75d7828d
Yannick De Oliveira <yannick.de-oliveira@inrae.fr>
2022-07-11 20:16:04 +0200

    add Menu app

--- _project/settings.py
+++ _project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recette',
-    'source'
+    'source',
+    'menu'
 ]
 
 MIDDLEWARE = [
---
+++ menu/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Menu(models.Model):
+    date = models.DateField(auto_now=True)
+    recettes = models.ManyToManyField('recette.Recette', through='MenuComposition')
+    
+    def __str__(self):
+        return "Menu du {0}".format(self.date)
+
+class MenuComposition(models.Model):
+    recette = models.ForeignKey('recette.Recette', on_delete=models.CASCADE)
+    menu = models.ForeignKey('Menu', on_delete=models.CASCADE)
+    portions = models.IntegerField()
