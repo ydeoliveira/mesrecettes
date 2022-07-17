62fc3c86c2bcdfe6c8dbd03b19b7b88e78c11e25
Yannick De Oliveira <yannick.de-oliveira@inrae.fr>
2022-07-17 17:32:38 +0200

    added css file

--- _project/settings.py
+++ _project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from corsheaders.defaults import default_headers
+from django.conf.global_settings import STATICFILES_DIRS
 
 CORS_ALLOW_HEADERS = default_headers + (
     'Access-Control-Allow-Origin',
@@ -127,7 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "static/"
+STATICFILES_DIRS = [BASE_DIR / "static/", 
+               ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
