e9de1fafe58c15d677044898fca5eac227947609
Yannick De Oliveira <yannick.deoliveira@gmail.com>
2023-03-11 13:51:55 +0100

    code repas dans la liste de courses

--- menu/views.py
+++ menu/views.py
@@ -19,8 +19,12 @@ class Courses(View):
         menu = Menu.objects.get(id=kwargs['pk'])
         courses = {}
         placard = set()
-        for repas in menu.menucomposition_set.all() :
-            for ingredient in repas.recette.listeingredients_set.all():
+        repas = {}
+        tags = "abcdefghijklmnopqrstuvwxyz"
+        i=0
+        for plat in menu.menucomposition_set.all() :
+            repas[tags[i]] = plat
+            for ingredient in plat.recette.listeingredients_set.all():
                 _rayon = ingredient.ingredient.get_rayon_display()
                 _ing = ingredient.ingredient
                 _unite = ingredient.unite
@@ -32,16 +36,18 @@ class Courses(View):
                     placard.add(_ing)
                 if _ing.placard == False and _unite not in courses[_rayon][_ing] :
                     courses[_rayon][_ing][_unite] = 0
-                
+                if _ing.placard == False and "0repas" not in courses[_rayon][_ing] :
+                    courses[_rayon][_ing]["0repas"] = []
                 if _ing.placard == False :
-                    courses[_rayon][_ing][_unite] += ingredient.quantite * repas.portions / ingredient.recette.nombre
-        
+                    courses[_rayon][_ing][_unite] += ingredient.quantite * plat.portions / ingredient.recette.nombre
+                    courses[_rayon][_ing]["0repas"].append(tags[i])
+            i+=1
         mode = request.GET.get("mode")
         
         if mode and mode=="print" :
-            return render(request, "courses-nocss.html", {'courses': courses, 'placard':placard, 'menu':menu})
+            return render(request, "courses-nocss.html", {'courses': courses, 'placard':placard, 'menu':menu, 'repas':repas})
         else :
-            return render(request, self.template_name, {'courses': courses, 'placard':placard, 'menu':menu})
+            return render(request, self.template_name, {'courses': courses, 'placard':placard, 'menu':menu, 'repas':repas})
 
 class GridMenu(View):
     template_name = "grid.html"
